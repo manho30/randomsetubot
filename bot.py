3f4046d8b29314bb4ccdab106ded67b6d9374972
manho <86037446+manho30@users.noreply.github.com>
2022-10-29 12:27:40 +0800

    update: simplify segment code
    commit: manho 
    <manho30@outlook.my>

--- bot.py
+++ bot.py
@@ -1,8 +1,9 @@
-import telebot
-import requests
 import json
 
-bot = telebot.TeleBot('5410945602:AAFg6NUHBoA2Oq4mzUNUmhvx-DC1B3_NYs8', parse_mode=None)
+import telebot
+import requests
+
+bot = telebot.TeleBot('TOKEN', parse_mode=None)
 
 @bot.message_handler(commands=['start'])
 def start(msg):
@@ -11,9 +12,6 @@ def start(msg):
 @bot.message_handler(content_types=['text'])
 def send_text(msg):
         bot.send_chat_action(msg.chat.id, 'upload_photo')
-        r = requests.get('https://api.lolicon.app/setu/v2')
-        data = json.loads(r.text)
-        pic = data['data'][0]['urls']['original']
-        bot.send_photo(msg.chat.id, pic, reply_to_message_id=msg.message_id)
+        bot.send_photo(msg.chat.id, json.loads(requests.get('https://api.lolicon.app/setu/v2'))['data'][0]['urls']['original'], reply_to_message_id=msg.message_id)
         
 bot.infinity_polling()
